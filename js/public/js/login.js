a9eb39ac7e7e5cd4e5b78c9aaa6ce3eb9b5bb914
dangngocbun <93512493+dangngocbun@users.noreply.github.com>
2022-12-20 19:16:56 +0700

    Add files via upload

---
+++ js/public/js/login.js
@@ -0,0 +1,40 @@
+const BASE_URL = 'http://localhost/api'
+function doLogin(){
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+
+    login(email, password)
+}
+
+function login(email, password){
+    let params = {'email': email,
+    'password': password}
+    $.post(BASE_URL + '/login.php', params, function(data){
+        let res = JSON.parse(data)
+        if(res.code == 200){
+            window.location = 'index.html'
+        }else{
+            alert('Login Failed')
+        }
+    })
+}
+function doRegister() {
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+
+    register(email, password)
+
+}
+
+function register(email, password) {
+    let params = { 'email': email, 'password': password }
+    $.post(BASE_URL +'/register.php', params, function (data) {
+        let res = JSON.parse(data)
+        if (res.code == true) {
+            window.location = 'login.html'
+        } else 
+        window.location = 'login.html'
+           alert('ok')
+            
+    })
+}
